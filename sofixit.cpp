66b2de20203cb03a715a009671515f468ca889cd
adam <apawlowski@fream.pl>
2023-10-24 02:12:39 +0200

    create first brute solution - beams

--- sofixit.cpp
+++ sofixit.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 
 long long material(std::vector<int> spaceship);
+long long material_brute(std::vector<int> spaceship);
 
 int main(){
     std::vector<int> spaceship;
@@ -29,6 +30,8 @@ int main(){
         spaceship.push_back(test[i]);
 
     std::cout << material(spaceship);
+    std::cout << std::endl;
+    std::cout << material_brute(spaceship);
     return 0;
 }
 
@@ -103,3 +106,39 @@ long long material(std::vector<int> spaceship){
     return water;
 }
 
+
+long long material_brute(std::vector<int> spaceship){
+    // idea is to shoot "beams" from both sides for each height. first, find the highest element:
+    int highest = 0;
+    for(int i = 0; i < spaceship.size(); i++)
+        highest = std::max(highest, spaceship[i]);
+    // std::cout<<"highest: " << highest << std::endl;
+    long long water = 0;
+    // shoot beams from left to right for each height
+    for(int i = highest; i >= 0; i--){
+        // std::cout << "i: " << i << std::endl;
+        // left beam 
+        int L = 0;
+        while(spaceship[L] < i && L < spaceship.size())
+            L++;
+        // right beam
+        int R = spaceship.size() - 1;
+        while(spaceship[R] < i && L >= 0)
+            R--;
+        // now we know that the water cannot exist where the beams are, otherwise it would flow out
+        // so we can calculate the volume of water between the beams and substract the total volume of the spaceship's parts in between
+        if (L > R) continue;
+        long long volume = (long long)(R - L - 1);
+        if(volume <= 0) continue;
+
+        // std::cout << "L, R: " << L << " " << R << "volume before: " << volume << std::endl;
+        for(int j = L + 1; j < R; j++){
+            // std::cout << spaceship[j] << "-";
+            if (spaceship[j] >= i)
+                volume--;
+        }
+        // std::cout << " volume after: " << volume << std::endl;
+        water += volume;
+    }
+    return water;
+}
